a505d9c0b64b51b70c415d95a7d94034bd4b9a5e
Sugar_F0x <sugarfox.dev@gmail.com>
2021-01-22 12:59:06 +0300

    :white_check_mark: Add bonx image as well as corresponding 404 text

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,6 @@
 <template lang="pug">
   v-app(dark)
-    v-app-bar(app)
-      router-link(
-        to="/"
-        style="text-decoration: none; color: inherit;"
-      )
-        v-toolbar-title Base Boilerplate
     v-main
-      v-container
+      v-container.fill-height
         nuxt
-    v-footer(
-      app
-      absolute
-    )
-      v-spacer
-      SugarMark
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,21 @@
 <template lang="pug">
-  v-row(
+  v-row.fill-height(
     justify="center"
     align="center"
+    no-gutters
   )
-    v-col(cols="8")
-      v-card
-        v-card-title This is the base boilerplate page
-        v-card-text This project was Sugar pre-configured
+    v-col(
+      cols="12"
+      sm="10"
+      md="8"
+      lg="7"
+    )
+      div.text-center.headline This isn't the page you are looking for
+      v-img(
+        :src="require('~/assets/img/fox.png')"
+        aspect-ratio="1"
+        contain
+      )
 </template>
 
 <script lang="ts">
