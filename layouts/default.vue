fe58dc0ecf73eacef03f4ec66f611d4c998122ac
Sugar_F0x <sugarfox.dev@gmail.com>
2021-01-22 12:47:32 +0300

    :milky_way: Initial setup
    > Configure Sugar Boilerplate

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  v-app(dark)
+    v-app-bar(app)
+      router-link(
+        to="/"
+        style="text-decoration: none; color: inherit;"
+      )
+        v-toolbar-title Base Boilerplate
+    v-main
+      v-container
+        nuxt
+    v-footer(
+      app
+      absolute
+    )
+      v-spacer
+      SugarMark
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  v-row(
+    justify="center"
+    align="center"
+  )
+    v-col(cols="8")
+      v-card
+        v-card-title This is the base boilerplate page
+        v-card-text This project was Sugar pre-configured
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'home'
+})
+</script>
